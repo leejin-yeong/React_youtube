22fc2b033822b138d568f3260ff3727b24d228a7
JinYoung Lee <cookie178@naver.com>
2020-09-13 12:06:30 +0900

    make index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"; 
+//깔려있는 module에서 가져옴
+//import 사용할 이름 from module이름
+import ReactDOM from "react-dom";
+/*나중에 설명
+import {HashRouter} from 'react-router-dom';
+*/
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+//같은 src 폴더 안에 js 파일들을 가져다 쓰겠다.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render( 
+    //여기서만 사용.
+    //다른 .js파일로는 class, component 만들어서 사용.
+    /*
+    <HashRouter>
+        <Header/>
+        <Body/>
+        <Footer/>
+    </HashRouter>
+    안에 있는 것도 가져와서 함께 rendering
+    하나로 싸져있어야 해서 일반적으론 div 태그 사용
+    */
+   document.querySelector("#container")
+   //container라는 id를 가진 곳에 렌더링을 할 것 이다.
+   //querySelector는 위치를 찾아주는 것.
+)
